7e4701d15e9daed85f85a822b1a396a93282f3a2
Nimit Shah <nimit@iloan.com>
2016-01-27 09:25:46 -0600

    Change upstart to iloan

--- spec/records/header_segment_spec.rb
+++ spec/records/header_segment_spec.rb
@@ -31,7 +31,7 @@ describe Metro2::Records::HeaderSegment do
         'Credit Reporter'.ljust(40, ' '),
         '123 Report Dr Address CA 91111'.ljust(96, ' '),
         '5555555555',
-        'Upstart Engineer metro 2 gem'.ljust(40, ' '),
+        'iLoan Engineer metro 2 gem'.ljust(40, ' '),
         '01104',
         ' ' * 156
       ]
